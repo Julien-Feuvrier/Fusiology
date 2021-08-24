b397abd3c3e88d97b9b0c72faaf2756f23f72242
Julien-Feuvrier <feuvrier-julien@orange.fr>
2021-08-24 23:18:41 +0200

    Clean the FABRIK algorithm and add comments.

--- Assets/Scripts/InverseKinematics/Fabrik.cs
+++ Assets/Scripts/InverseKinematics/Fabrik.cs
@@ -3,21 +3,61 @@ using UnityEngine;
 
 namespace Fusiology.InverseKinematics
 {
+    /// <summary>
+    /// Implementation of the FABRIK algorithm which determines the positions of an interconnected joints set through inverse kinematics.
+    /// cf http://andreasaristidou.com/FABRIK.html
+    /// </summary>
+    /// <seealso cref="UnityEngine.MonoBehaviour" />
     public class Fabrik : MonoBehaviour
     {
+        #region Private inspector fields
+
+        /// <summary>
+        /// The set of the interconnected joints.
+        /// They are sorted by hierarchy, from the fixed origin joint (first <see cref="Transform"/>) to the edge joint (last <see cref="Transform"/>).
+        /// </summary>
         [SerializeField]
         private Transform[] m_Joints;
 
+        /// <summary>
+        /// The target <see cref="Transform"/> on which the edge joint should be attached to.
+        /// </summary>
         [SerializeField]
         private Transform m_Target;
 
+        /// <summary>
+        /// The tolerance to use in the FABRIK algorithm.
+        /// It corresponds to the distance tolerance between <see cref="m_Target"/> and the edge joint bellow which the FABRIK algorithm stops.
+        /// </summary>
         [SerializeField]
-        private float m_Tolerance = 0.001f;
+        private float m_Tolerance = 0.01f;
 
+        #endregion Private inspector fields
+
+        #region Private fields
+
+        /// <summary>
+        /// The joints positions array which is preallocated to avoid multiple allocations in <see cref="Run"/>.
+        /// </summary>
         private Vector3[] m_JointsPositions;
 
+        #endregion Private fields
+
+        #region Life-cycle methods
+
+        /// <summary>
+        /// Run the FABRIK algorithm each frame when this component is enabled.
+        /// </summary>
         private void Update() => Run();
 
+        #endregion Life-cycle methods
+
+        #region FABRIK methods
+
+        /// <summary>
+        /// Run the FABRIK algorithm which updates the positions of all the <see cref="m_Joints"/> in order to move the edge joint on the <see cref="m_Target"/>.
+        /// </summary>
+        [ContextMenu(nameof(Run))]
         private void Run()
         {
             if (m_JointsPositions == null || m_JointsPositions.Length != m_Joints.Length)
@@ -82,5 +122,7 @@ namespace Fusiology.InverseKinematics
                 m_Joints[i].SetPositionAndRotation(m_JointsPositions[i], Quaternion.FromToRotation(Vector3.up, m_JointsPositions[i + 1] - m_JointsPositions[i]));
             }
         }
+
+        #endregion FABRIK methods
     }
 }
